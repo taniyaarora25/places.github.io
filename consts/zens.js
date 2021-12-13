5d10087fd0e34d7f1148e77894270bdb5f8be9b7
taniyaarora25 <taniyarora1@gmail.com>
2021-12-13 21:53:58 +0530

    commit

---
+++ consts/zens.js
@@ -0,0 +1,26 @@
+const zens = [
+    {   id: 1,
+        mantra: "Find your inner motherforking peace",
+        image: "images/under.jpg",
+        audio: "sounds/waves.mp3",
+        text: "I'm feeling beachy keen",
+    },
+    {   id: 2,
+        mantra: "Breathe in peace, breathe out bullshirt",
+        image: "images/sweet.jpg",
+        audio: "sounds/birds.mp3",
+        text: "Ain't no mountain high enough",
+    },
+    {   id: 3,
+        mantra: "Benches will be benches",
+        image: "images/library.jpg",
+        audio: "sounds/chimes.mp3",
+        text: "I want a baguette",
+    },
+    {   id: 4,
+        image: "images/office3.jpg",
+        text: "Home"
+    }
+]
+
+export default zens;
